349b9af4791c1376fe1b6b0ff61725e238be52c8
Zack Bumpous <upwork.ztb@gmail.com>
2019-09-19 16:26:44 -0500

    created my person class aka my base class and gave it its props and just finishing its speak method

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,16 @@
 // CODE here for your Lambda Classes
+//base class Person below
+class Person = {
+    constructor(personAttributes){
+        //person receives name age and location as props
+        this.name = personAttributes.name;
+        this.age = personAttributes.age;
+        this.location = personAttributes.location;
+    }
+    //methods go here
+    //speak method below
+    speak(){
+        return `Hello my name is ${this.name}, I am from Bedrock`
+    }
+
+}// closes person
